3667a708c897094bff6e1a9169a216f40b97b2a4
Monish-R45 <81694620+Monish-R45@users.noreply.github.com>
2021-06-30 21:09:28 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,26 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png")
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      this.visibility=255
+      if(this.body.speed<3){
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      }else{
+        push();
+        this.visibility=this.visibility-5
+        tint(255,this.visibility)
+        pop();
+      }
+    }
+}
